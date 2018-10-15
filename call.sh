72c25a96ebed726a5da023b580c6f7df666b720f
badrus123 <badrussholehaxel@gmail.com>
2018-10-16 01:56:49 +0700

    adit asu

--- call.sh
+++ call.sh
@@ -5,26 +5,21 @@ echo Selamat datang kak, Siapa nick kaka? #tulisan keluar
 
 read nick #membaca yang ditulis
 echo "================================================="
-echo "== ╦ ╦┌─┐┌─┐┬┌─┌─┐┬─┐  ┬┌┐┌┌┬┐┌─┐┌┐┌┌─┐┌─┐┬┌─┐ 	ucup   =="
-echo "== ╠═╣├─┤│  ├┴┐├┤ ├┬┘  ││││ │││ ││││├┤ └─┐│├─┤ Telkom University=="
-echo "== ╩ ╩┴ ┴└─┘┴ ┴└─┘┴└─  ┴┘└┘─┴┘└─┘┘└┘└─┘└─┘┴┴ ┴ 	1301164057			==     "
+echo "== ╦ ╦┌─┐┌─┐┬┌─┌─┐┬─┐  ┬┌┐┌┌┬┐┌─┐┌┐┌┌─┐┌─┐┬┌─┐ 	  Aditya Eka Maulana   		=="
+echo "== ╠═╣├─┤│  ├┴┐├┤ ├┬┘  ││││ │││ ││││├┤ └─┐│├─┤ 	Hacker Telkom University	=="
+echo "== ╩ ╩┴ ┴└─┘┴ ┴└─┘┴└─  ┴┘└┘─┴┘└─┘┘└┘└─┘└─┘┴┴ ┴ 		hacker terjago			=="
 echo "================================================="
 echo selamat datang $nick ":)"
 echo
-echo "Silahkan masukan nomor telp target"
-echo contoh 0812345678
+echo "Silahkan masukan nomor ip target"
+echo contoh 192.168.36.1
 read target # masukin no telp
-
 echo
 echo Apakah nomor $target "sudah benar?"
 echo y/n?
 read confirm
 echo
 if [ $confirm = "y" ]; then
-echo Melakukan spam call ke nomor $target
-target_do='http://zpro.000webhostapp.com/api/call.php?nomor='$target
-CURL_RESPONSE=`curl -s -o /dev/null -w "%{http_code}" $target_do`
-#echo $target_do
-else
+echo yah anda ketipu boss maaf nih aditya eka maulana emang ngeselin
 echo Kesalahan, silahkan coba lagi
 fi
