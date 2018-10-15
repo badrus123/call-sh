6a7448b5434a16a92434ab87d2e79f64a6ec4f01
badrus123 <badrussholehaxel@gmail.com>
2018-10-15 20:42:39 +0700

    first commit

---
+++ call.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#hacker indonesia
+
+echo Selamat datang kak, Siapa nick kaka? #tulisan keluar
+
+read nick #membaca yang ditulis
+echo "================================================="
+echo "== ╦ ╦┌─┐┌─┐┬┌─┌─┐┬─┐  ┬┌┐┌┌┬┐┌─┐┌┐┌┌─┐┌─┐┬┌─┐ 	ucup   =="
+echo "== ╠═╣├─┤│  ├┴┐├┤ ├┬┘  ││││ │││ ││││├┤ └─┐│├─┤ Telkom University=="
+echo "== ╩ ╩┴ ┴└─┘┴ ┴└─┘┴└─  ┴┘└┘─┴┘└─┘┘└┘└─┘└─┘┴┴ ┴ 	1301164057			==     "
+echo "================================================="
+echo selamat datang $nick ":)"
+echo
+echo "Silahkan masukan nomor telp target"
+echo contoh 0812345678
+read target # masukin no telp
+
+echo
+echo Apakah nomor $target "sudah benar?"
+echo y/n?
+read confirm
+echo
+if [ $confirm = "y" ]; then
+echo Melakukan spam call ke nomor $target
+target_do='http://zpro.000webhostapp.com/api/call.php?nomor='$target
+CURL_RESPONSE=`curl -s -o /dev/null -w "%{http_code}" $target_do`
+#echo $target_do
+else
+echo Kesalahan, silahkan coba lagi
+fi
